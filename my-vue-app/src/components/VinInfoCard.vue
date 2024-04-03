edb5f58501edff2c945e1cf66c19d2c9fb022397
Luganito <63515050+luganitoo@users.noreply.github.com>
2024-04-03 17:55:52 -0300

    VinInfoCard now displays the Car location with map and remaining charge

--- my-vue-app/src/components/VinInfoCard.vue
+++ my-vue-app/src/components/VinInfoCard.vue
@@ -9,20 +9,43 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { MapCard } from 'vue-map-card'
+import { ZapIcon } from 'lucide-vue-next'
 
 const store = useStore();
 const cardData = computed(() => store.cardData);
+const vehicledata = computed(() => cardData.value?.vehicle_data);
+const location = computed(() => cardData.value?.location);
 
 </script>
 
 <template>
   <Card >
     <CardHeader>
-      <CardTitle>Vin Details</CardTitle>
+      <CardTitle>Car Location</CardTitle>
       <CardDescription>Vehicle Information</CardDescription>
     </CardHeader>
     <CardContent class="w-[400px]">
-        {{cardData}}
+      <div class="text-xl font-bold flex justify-center ">
+        <img :src="'https://flagcdn.com/' + location.country_code + '.svg'" class="h-6 "/>
+      </div>
+      <div class="text-xl font-bold ">
+        {{ location?.city }}, {{ location?.country }}
+      </div>
+      <p class="text-xs text-muted-foreground ">
+        ({{ vehicledata?.latitude }}, {{ vehicledata?.longitude }})
+      </p>
+      <div class="flex justify-center text-2xl font-bold py-2">
+        <ZapIcon class="pt-2 text-green-500" size="25"/> {{ parseInt(vehicledata?.remaining_electrical_range) }} miles remaining
+      </div>
+
+
+      <!-- {{ vehicledata }} -->
+      
+      <MapCard 
+      :city="location?.city"
+      :country="location?.country"
+      />
     </CardContent>
     <!-- <CardFooter>
       Card Footer
