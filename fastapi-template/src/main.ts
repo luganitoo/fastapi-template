0c1c30be6eea76e73e2524141f68463acab17449
Luganito <63515050+luganitoo@users.noreply.github.com>
2024-04-03 15:56:29 -0300

    readded env

--- fastapi-template/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './style.css'
-import App from './App.vue'
-
-const pinia = createPinia()
-
-createApp(App).use(pinia)
-createApp(App).mount('#app')
